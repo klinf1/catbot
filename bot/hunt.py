ddbf1ed5db00c61aab3fc763f5d701d20fd43ab5
klinf <klinf157@gmail.com>
2025-10-04 14:56:37 +0300

    hunt supports multi word name

--- bot/hunt.py
+++ bot/hunt.py
@@ -15,7 +15,7 @@ class HuntCommandHandler(CommandBase):
         super().__init__(update, context)
 
     async def hunt(self):
-        params = capitalize_for_db(self.text.strip().split(" ", 1))
+        params = capitalize_for_db(self.text.strip().split("\n", 1))
         try:
             main_logger.debug(f"Начало охоты для {self.user.username} {params}")
             prey, success = Hunt(
